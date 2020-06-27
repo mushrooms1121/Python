6f3e942aae319167c24a42f056655b1c4c3b0e4c
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-06-27 16:00:39 +0800

    Add files via upload

---
+++ 澎澎/L2 (變數與資料型態)/變數型態.py
@@ -0,0 +1,17 @@
+#變數: 用來儲存資料的自訂名稱，用英文，開頭不能是數字
+#變數名稱 = 資料
+x = 3
+
+#print(資料)
+print(3)
+print(True)
+print([1,2,3])
+print(x)
+
+#取代舊的資料
+x = True
+print(x)
+x = "Hello"
+print(x)
+x = {3,4,5} #集合set
+print(x)
